a2bb9648d3617e54f58a3df91e38748f9a6514ad
evan <langhunm@gmail.com>
2017-04-21 07:26:44 +0800

    1

--- pack/pytest.py
+++ pack/pytest.py
@@ -3,7 +3,15 @@ Created on Apr 20, 2017
 
 @author: evan
 '''
-print "hello python!"
-print "hello python!"
-print "hello python!"
-print "hello python!"
+import urllib2
+respon=urllib2.urlopen("http://www.baidu.com")
+# print respon.read()
+request=urllib2.Request("http://www.qq.com")
+respon1=urllib2.urlopen(request)
+# print respon1.read()
+def printstr(s11tr):
+    
+    "adfadsfasdssss"
+    print s11tr
+    return 
+printstr(  "adfadsfasd")
